5a27fae560fe970eb3340e8c231c88e67957e033
ayselallam <aysel.allam@student.giu-uni.de>
2025-11-11 09:18:29 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,7 +4,22 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+class course{
+private:
+string coursecode;
+string coursename;
+int maxStudents;
+int currentStudents;
+
+public:
+  addStudent( const Student& s){
+    student= new student;
+   displayCourseInfo(){
+    
+   }
+  }
+
+}
 
 
 
