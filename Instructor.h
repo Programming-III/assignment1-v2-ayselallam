f0fd911286e1b417b27f13888072adc325f42624
ayselallam <aysel.allam@student.giu-uni.de>
2025-11-11 09:46:32 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -3,7 +3,29 @@
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
+
+#ifndef COURSE_H
+#define COURSE_H
+
+#include <string>
+#include "Student.h"
+using namespace std;
+class course{
+private:
+string coursecode;
+string coursename;
+int maxStudents;
+int currentStudents;
+
+public:
+  addStudent( const Student& s){
+    student= new student;
+   displayCourseInfo(){
+    
+   }
+  }
+
+}
 
 
 
